ad8f2552a5a34528184fcbbf592be04564ab2c47
hdzalejandro24-wq <hdzalejandro24@gmail.com>
2025-11-03 16:25:55 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author carpi
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // 3 parametros
+        // variable y valor de inicio
+        // hasta donde llegas  CONDICION--> TIENE QUE SE VERDADERO 
+        // incremento o decremento
+        // SUMAR O RESTAR:
+      // OPERACIONES: AÑADIR 1 O RESTAR 1
+      // ++ --
+      // i++ SIGNIFICA SUMARLE 1 AL VALOR DE LAi
+        for(int i = 1; i <= 10; i++){
+         System.out.println("HOLA MUNDO");
+        for(int i = 10; i >= 1; i--){
+              }
+            // i SIGNIFICA RESTABLE 1 VALOR 1 
+            System.out.println("HOLA MUNDO");
+                          
+        }
+    }
+}
+   
+
